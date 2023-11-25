d354afddf94d61fb8b03543ee28b7a9d1bce27bd
이경용 <sleekydz86@icloud.com>
2023-11-25 15:53:19 +0900

    mac os환경에 맞춰 미니 채팅 셋팅
    mac os환경에 맞춰 미니 채팅 셋팅

---
+++ src/main/java/com/mini/chatstudy/ChatstudyApplication.java
@@ -0,0 +1,13 @@
+package com.mini.chatstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatstudyApplication.class, args);
+	}
+
+}
