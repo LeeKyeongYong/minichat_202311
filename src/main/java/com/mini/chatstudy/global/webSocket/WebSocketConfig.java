71712b5530e2bf524819cd6cc9764666fc5c6db9
이경용 <sleekydz86@icloud.com>
2023-12-01 12:45:38 +0900

    패스 오기정정
    패스오기 정정

--- src/main/java/com/mini/chatstudy/global/webSocket/WebSocketConfig.java
+++ src/main/java/com/mini/chatstudy/global/webSocket/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
